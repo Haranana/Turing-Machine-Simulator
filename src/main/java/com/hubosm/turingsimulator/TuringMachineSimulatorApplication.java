e41e8fc9901c17d0560bbdcedcd9f548f33631a4
Haranana <hubert.osmalek@op.pl>
2025-06-19 13:23:03 +0200

    added starter project

---
+++ src/main/java/com/hubosm/turingsimulator/TuringMachineSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.hubosm.turingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringMachineSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringMachineSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubosm/turingsimulator/TuringMachineSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubosm.turingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringMachineSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
